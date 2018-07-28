19a767523dd8776ac2a05991387dda64c262282b
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-28 20:25:25 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,11 +8,7 @@ WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8
 
 def empty_board?(board)
   board.all? do|item|
-    if(item.nil?)
-      return false
-    else
-      return true
-    end
+    item.nil?
   end
 end
 
