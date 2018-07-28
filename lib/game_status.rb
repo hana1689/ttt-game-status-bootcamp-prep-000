a482c08ada462d1ffff25ae65c643e26f0804083
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-28 20:18:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,8 +24,7 @@ def won?(board)
       position_1 = board[win_index_1]
       position_2 = board[win_index_2]
       position_3 = board[win_index_3]
-      print(win_index_1, win_index_2, win_index_3)
-      print(position_1, position_2, position_3)
+      print(win_combination)
       if(position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O")
         return win_combination
       else
